52d050b50903460a7238d7f0cbde6877a5b467f1
Emanon <moemanon@pm.me>
2024-01-08 22:51:16 +0100

    feat: add fstrim service and luks discards + speedy things

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,12 @@
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-69f07c4f-4348-468f-8dd2-caff60fd31fe".device = "/dev/disk/by-uuid/69f07c4f-4348-468f-8dd2-caff60fd31fe";
+  boot.initrd.luks.devices."luks-69f07c4f-4348-468f-8dd2-caff60fd31fe" = {
+    device = "/dev/disk/by-uuid/69f07c4f-4348-468f-8dd2-caff60fd31fe";
+    allowDiscards = true;
+    bypassWorkqueues = true;
+  };
+
   users.users.emanon.shell = pkgs.zsh;
   programs.zsh.enable = true;
   programs.ssh.startAgent = true;
