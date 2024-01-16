30175c61fe970ee3e17f1e8afefe7109d43955e1
Emanon <moemanon@pm.me>
2024-01-16 18:48:02 +0100

    chore: refactor file, delete defaults comments

--- home.nix
+++ home.nix
@@ -6,7 +6,9 @@
   home.username = "emanon";
   home.homeDirectory = "/home/emanon";
 
-  programs.starship.enable = true;
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -21,6 +23,7 @@
     };
   };
 
+  programs.starship.enable = true;
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
@@ -43,48 +46,16 @@
     enable = true;
   };
 
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-  programs.home-manager.enable = true;
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     alacritty
     starship
     htop
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".ssh/allowed_signers".text = "* ${builtins.readFile /home/emanon/.ssh/id_ed25519.pub}";
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/emanon/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
