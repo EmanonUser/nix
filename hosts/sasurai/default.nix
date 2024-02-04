541109c90044eba33212dd1e40db8a8e1df84679
Emanon <moemanon@pm.me>
2024-02-04 21:25:39 +0100

    fix: enable steam

--- hosts/sasurai/default.nix
+++ hosts/sasurai/default.nix
@@ -79,6 +79,12 @@
     pulse.enable = true;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   users.users.emanon = {
     isNormalUser = true;
     description = "emanon";
