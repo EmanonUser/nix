3704f6ac03cd7612091f002170b80dc4c9bd863d
Emanon <moemanon@pm.me>
2024-03-13 20:14:03 +0100

    feat: remove border and enable compact mode

--- modules/zellij/zellij.nix
+++ modules/zellij/zellij.nix
@@ -7,6 +7,10 @@
     programs.zellij = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+        pane_frames = false;
+        default_layout = "compact";
+      };
     };
   };
 }
