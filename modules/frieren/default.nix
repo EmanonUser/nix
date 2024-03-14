80e0088a6e97bd0cd4fa161da790e4654ffc6cda
Emanon <moemanon@pm.me>
2024-03-14 21:35:17 +0100

    fix: modules path

--- modules/frieren/default.nix
+++ modules/frieren/default.nix
@@ -1,16 +1,16 @@
 {nixvim, ...}: {
   imports = [
-    nixvim.nixosModules.nixvim
-    ./home/alacritty/alacritty.nix
-    ./home/atuin/atuin.nix
-    ./home/git/git.nix
-    ./home/neovim/neovim.nix
-    ./home/packages/home-packages.nix
-    ./home/settings/home-manager-settings.nix
-    ./home/ssh/ssh.nix
-    ./home/starship/starship.nix
-    ./home/zellij/zellij.nix
-    ./home/zoxide/zoxide.nix
-    ./home/zsh/zsh.nix
+    nixvim.homeManagerModules.nixvim
+    ../home/alacritty/alacritty.nix
+    ../home/atuin/atuin.nix
+    ../home/git/git.nix
+    ../home/neovim/neovim.nix
+    ../home/packages/home-packages.nix
+    ../home/settings/home-manager-settings.nix
+    ../home/ssh/ssh.nix
+    ../home/starship/starship.nix
+    ../home/zellij/zellij.nix
+    ../home/zoxide/zoxide.nix
+    ../home/zsh/zsh.nix
   ];
 }
