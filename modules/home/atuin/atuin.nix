0a99c9d0136620fb150be9fe4e491cc84cf9ed3e
Emanon <moemanon@pm.me>
2024-03-15 18:06:20 +0100

    feat: config atuin

--- modules/home/atuin/atuin.nix
+++ modules/home/atuin/atuin.nix
@@ -7,5 +7,10 @@
     enable = true;
     enableZshIntegration = true;
     flags = ["--disable-up-arrow"];
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      keymap_mode = "vim-normal";
+    };
   };
 }
