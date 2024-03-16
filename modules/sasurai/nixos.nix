3f59d961e13d088ffce7f379162232392161cc6e
Emanon <moemanon@pm.me>
2024-03-16 11:20:35 +0100

    fix: forgot a bunch of nixos modules

--- modules/sasurai/nixos.nix
+++ modules/sasurai/nixos.nix
@@ -1,8 +1,11 @@
 {
   imports = [
     ../nixos/core
+    ../nixos/services/services.nix
     ../nixos/hardware
     ../nixos/virtualisation
+    ../nixos/greetd/greetd.nix
+    ../nixos/steam/steam.nix
     ./nix-packages
   ];
 }
