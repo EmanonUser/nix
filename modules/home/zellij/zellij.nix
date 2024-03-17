719c7464e0acbd177a7229a4ec63ea63355e919c
Emanon <moemanon@pm.me>
2024-03-17 10:33:45 +0100

    feat: do not start an zellij session when using ssh

--- modules/home/zellij/zellij.nix
+++ modules/home/zellij/zellij.nix
@@ -1,10 +1,34 @@
 {
   programs.zellij = {
     enable = true;
-    enableZshIntegration = true;
+    enableZshIntegration = false;
     settings = {
       pane_frames = false;
       default_layout = "compact";
     };
   };
+
+  programs.zsh.initExtra = ''
+      if [[ -z "$ZELLIJ" ]]; then
+      if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+        # Don't attach to Zellij if SSH connection
+        if [[ -n "$SSH_CLIENT" ]]; then
+          echo "Skipping Zellij - SSH connection detected"
+        else
+          zellij attach -c
+        fi
+      else
+        # Don't start Zellij if SSH connection
+        if [[ -n "$SSH_CLIENT" ]]; then
+          echo "Skipping Zellij - SSH connection detected"
+        else
+          zellij
+        fi
+      fi
+
+      if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+        exit
+      fi
+    fi
+  '';
 }
