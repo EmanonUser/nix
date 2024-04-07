12522e4bdb53dbe76f5622ada1a01afe18ad337d
Emanon <moemanon@pm.me>
2024-04-07 11:29:42 +0200

    feat: steam overrides

--- modules/nixos/steam/steam.nix
+++ modules/nixos/steam/steam.nix
@@ -15,4 +15,22 @@
       pkgs.proton-ge-bin
     ];
   };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    };
+  };
 }
