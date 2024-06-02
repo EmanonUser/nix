4a657d470804c14d857345b05ae129c953b2b950
Emanon <moemanon@pm.me>
2024-06-02 09:55:59 +0200

    feat: disable zellij auto attach on tty(x)s

--- modules/home/zellij/zellij.nix
+++ modules/home/zellij/zellij.nix
@@ -10,25 +10,25 @@
 
   programs.zsh.initExtra = ''
       if [[ -z "$ZELLIJ" ]]; then
-      if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-        # Don't attach to Zellij if SSH connection
-        if [[ -n "$SSH_CLIENT" ]]; then
-          echo "Skipping Zellij - SSH connection detected"
+        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+            # Don't attach to Zellij if SSH connection or if attached to any /dev/tty(x)
+            if [[ -n "$SSH_CLIENT" ]] || [[ "$(tty)" =~ ^/dev/tty[0-9]+$ ]]; then
+                echo "Skipping Zellij - SSH connection or /dev/tty(x) detected"
+            else
+                zellij attach -c
+            fi
         else
-          zellij attach -c
+            # Don't start Zellij if SSH connection or if attached to any /dev/tty(x)
+            if [[ -n "$SSH_CLIENT" ]] || [[ "$(tty)" =~ ^/dev/tty[0-9]+$ ]]; then
+                echo "Skipping Zellij - SSH connection or /dev/tty(x) detected"
+            else
+                zellij
+            fi
         fi
-      else
-        # Don't start Zellij if SSH connection
-        if [[ -n "$SSH_CLIENT" ]]; then
-          echo "Skipping Zellij - SSH connection detected"
-        else
-          zellij
-        fi
-      fi
 
-      if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
-        exit
-      fi
+        if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+        fi
     fi
   '';
 }
