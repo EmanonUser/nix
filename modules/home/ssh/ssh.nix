fe094a7473f5b48676f2ae5848c9a24afd2acbc2
Emanon <moemanon@pm.me>
2024-07-02 19:02:32 +0200

    feat: add ssh configs

--- modules/home/ssh/ssh.nix
+++ modules/home/ssh/ssh.nix
@@ -1,10 +1,31 @@
 {
   home.file.".ssh/config".text = ''
+    User emanon
+    Port 22
     Host s1
       Hostname s1.home.arpa
-      Port 22
       KexAlgorithms +diffie-hellman-group1-sha1
       PubkeyAcceptedAlgorithms +ssh-rsa
       HostkeyAlgorithms +ssh-rsa
+
+    Host s1
+      Hostname r1.home.arpa
+
+    Host sasurai
+      Hostname sasurai.home.arpa
+
+    Host frieren
+      Hostname frieren.home.arpa
+
+    Host ubnt01
+      Hostname ubnt01.home.arpa
+
+    Host ubnt02
+      Hostname ubnt02.home.arpa
+
+
+    Host ctrlf
+      Hostname ctrl-f.io
+      User ctrlf
   '';
 }
