0f3c2eadea545547410bbb793c468ead46fb1ffc
Emanon <moemanon@pm.me>
2024-07-05 16:47:17 +0200

    feat: update ssh config

--- modules/home/ssh/ssh.nix
+++ modules/home/ssh/ssh.nix
@@ -1,28 +1,46 @@
 {
   home.file.".ssh/config".text = ''
-    User emanon
-    Port 22
+    Host sasurai
+      User emanon
+      Port 22
+      Hostname sasurai.home.arpa
+      IdentitiesOnly yes
+      IdentityFile /home/emanon/.ssh/id_ed25519
+      CertificateFile /home/emanon/.ssh/id_ed25519-cert.pub
+
+    Host frieren
+      User emanon
+      Port 22
+      Hostname frieren.home.arpa
+      IdentitiesOnly yes
+      IdentityFile /home/emanon/.ssh/id_ed25519
+      CertificateFile /home/emanon/.ssh/id_ed25519-cert.pub
+
+    Host r1
+      User emanon
+      Port 22
+      Hostname frieren.home.arpa
+      IdentitiesOnly yes
+      IdentityFile /home/emanon/.ssh/id_ed25519
+      CertificateFile /home/emanon/.ssh/id_ed25519-cert.pub
+
     Host s1
+      User emanon
+      Port 22
       Hostname s1.home.arpa
       KexAlgorithms +diffie-hellman-group1-sha1
       PubkeyAcceptedAlgorithms +ssh-rsa
       HostkeyAlgorithms +ssh-rsa
 
-    Host s1
-      Hostname r1.home.arpa
-
-    Host sasurai
-      Hostname sasurai.home.arpa
-
-    Host frieren
-      Hostname frieren.home.arpa
-
     Host ubnt01
+      User emanon
+      Port 22
       Hostname ubnt01.home.arpa
 
     Host ubnt02
+      User emanon
+      Port 22
       Hostname ubnt02.home.arpa
-
 
     Host ctrlf
       Hostname ctrl-f.io
