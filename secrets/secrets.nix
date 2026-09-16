30d4dc0c30b04035fd994949ca6b38a9321a9fa3
Emanon <moemanon@pm.me>
2026-09-16 14:28:04 +0200

    fix: re-encrypt agenix secrets for new vm host key (ZFS redeploy)

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   fern = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaTLWC5KirziNieTZw7LFPkuKDhnJKgjqs93TJVJQ1l emanon@fern";
-  vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYhltOYRffAqI94mfLbz3veubjH+A7EfOlHe4K0tuhk root@nixos-vm";
+  vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6btmu06DEv1BSicz0bD5IObx0Zj0/pZVwBShq0iRMO root@nixos-vm";
   sasurai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMerlJ+NA2avmCP0fTStNhF/7ZWTvxmGtT4aivtZ2slq emanon@sasurai";
   zoltraak = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMerlJ+NA2avmCP0fTStNhF/7ZWTvxmGtT4aivtZ2slq emanon@sasurai";
 in {
