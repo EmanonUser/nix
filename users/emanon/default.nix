7454c92377e8e0d5d42812debf12f7e59e93add5
Emanon <moemanon@pm.me>
2026-09-16 20:13:05 +0200

    feat(users): passwordless sudo for emanon

--- users/emanon/default.nix
+++ users/emanon/default.nix
@@ -12,4 +12,17 @@
   };
 
   programs.zsh.enable = true;
+
+  # Passwordless sudo for the emanon user.
+  security.sudo.extraRules = [
+    {
+      users = [username];
+      commands = [
+        {
+          command = "ALL";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
 }
