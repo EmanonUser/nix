ee18e36393cccc143567a760f98a0a803adbf3bf
Emanon <moemanon@pm.me>
2026-09-17 15:39:37 +0200

    feat(niri): autologin default, Noctalia Greeter as option, lock keybind

--- modules/nixos/niri/default.nix
+++ modules/nixos/niri/default.nix
@@ -1,45 +1,73 @@
 {
   config,
+  lib,
   pkgs,
   username,
   ...
-}: {
-  # niri, the scrollable-tiling Wayland compositor.
-  # https://github.com/YaLTeR/niri
-  programs.niri = {
-    enable = true;
-    useNautilus = false; # not needed, avoids pulling nautilus
+}: let
+  cfg = config.services.niri;
+in {
+  options.services.niri.autologin = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = ''
+      Boot straight into niri as `${username}`. When false, show the Noctalia
+      Greeter login screen first instead. Session locking (Noctalia shell's own
+      lock screen) works either way.
+    '';
   };
 
-  # Allow X11 apps (chrome, discord, ...) under niri.
-  programs.xwayland.enable = true;
-
-  # Make user-profile binaries (kitty, ghostty, ...) available in niri binds.
-  environment.sessionVariables.PATH = [
-    "${config.home-manager.users.${username}.home.profileDirectory}/bin"
-  ];
-
-  # Noctalia Greeter: greetd-based login screen, defaults to niri + `username`.
-  services.displayManager.noctalia-greeter = {
-    enable = true;
-    settings = {
-      session.default = "niri";
-      user.default = username;
-      keyboard = {
-        layout = "fr";
-        numlock = true;
+  config = lib.mkMerge [
+    {
+      # niri, the scrollable-tiling Wayland compositor.
+      # https://github.com/YaLTeR/niri
+      programs.niri = {
+        enable = true;
+        useNautilus = false; # not needed, avoids pulling nautilus
       };
-    };
-  };
 
-  # greetd runs the greeter as this dedicated system user.
-  services.greetd.settings.default_session.user = "greeter";
+      # Allow X11 apps (chrome, discord, ...) under niri.
+      programs.xwayland.enable = true;
 
-  users.groups.greeter = {};
+      # Make user-profile binaries (kitty, ghostty, ...) available in niri binds.
+      environment.sessionVariables.PATH = [
+        "${config.home-manager.users.${username}.home.profileDirectory}/bin"
+      ];
+    }
 
-  users.users.greeter = {
-    isSystemUser = true;
-    group = "greeter";
-    home = "/var/lib/noctalia-greeter";
-  };
+    (lib.mkIf cfg.autologin {
+      services.greetd = {
+        enable = true;
+        settings.default_session = {
+          user = username;
+          command = "${pkgs.niri}/bin/niri-session";
+        };
+      };
+    })
+
+    (lib.mkIf (!cfg.autologin) {
+      # Noctalia Greeter: greetd-based login screen, defaults to niri + `username`.
+      services.displayManager.noctalia-greeter = {
+        enable = true;
+        settings = {
+          session.default = "niri";
+          user.default = username;
+          keyboard = {
+            layout = "fr";
+            numlock = true;
+          };
+        };
+      };
+
+      # greetd runs the greeter as this dedicated system user.
+      services.greetd.settings.default_session.user = "greeter";
+      users.groups.greeter = {};
+
+      users.users.greeter = {
+        isSystemUser = true;
+        group = "greeter";
+        home = "/var/lib/noctalia-greeter";
+      };
+    })
+  ];
 }
