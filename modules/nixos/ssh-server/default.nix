f1cbd77261356ef639dae376b0f1e27767794e95
Emanon <moemanon@pm.me>
2026-09-18 11:19:55 +0200

    feat(ssh): deploy user CA to /etc/ssh and pin host CA in known_hosts
    - Trust /etc/ssh/user_ca.pub on every ssh-server host (sasurai, zoltraak,
      nixos-vm) instead of the emanon_user_ca.pub name
    - Write an @cert-authority line for *.home.arpa / *.emanon.dev to
      ~/.ssh/known_hosts on every host (incl. standalone home-manager hosts)
    - Drop the superseded modules/ssh/user_ca.pub (moved to ssh-server)

--- modules/nixos/ssh-server/default.nix
+++ modules/nixos/ssh-server/default.nix
@@ -4,9 +4,9 @@
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = false;
 
-  environment.etc."ssh/emanon_user_ca.pub".source = ./user_ca.pub;
+  environment.etc."ssh/user_ca.pub".source = ./user_ca.pub;
 
-  services.openssh.settings.TrustedUserCAKeys = "/etc/ssh/emanon_user_ca.pub";
+  services.openssh.settings.TrustedUserCAKeys = "/etc/ssh/user_ca.pub";
   services.openssh.extraConfig = ''
     HostCertificate /etc/ssh/ssh_host_ed25519_key-cert.pub
   '';
--- modules/ssh/ssh.nix
+++ modules/ssh/ssh.nix
@@ -1,5 +1,12 @@
-{
-  home.file.".ssh/config" = {
-    source = ./config;
-  };
+{ lib, ... }:
+let
+  hostCa = builtins.readFile ./host_ca.pub;
+  certAuthority =
+    "@cert-authority \"*.home.arpa,*.emanon.dev\" "
+    + lib.concatStringsSep " " (lib.take 2 (lib.splitString " " hostCa))
+    + "\n";
+in {
+  home.file.".ssh/config".source = ./config;
+
+  home.file.".ssh/known_hosts".text = certAuthority;
 }
