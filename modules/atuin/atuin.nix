b28c425567d43a2cf12c25344cd31f52d6d44274
Emanon <moemanon@pm.me>
2026-09-22 16:07:32 +0200

    feat: disable atuin AI mode

--- modules/atuin/atuin.nix
+++ modules/atuin/atuin.nix
@@ -12,9 +12,6 @@
         enabled = true;
         autostart = true;
       };
-ai = {
-        enabled = true;
-      };
       # Auto-login: session + key files are managed via age secrets landing at
       # /run/agenix (NixOS-host and home-manager agenix both default there).
       session_path = "/run/agenix/atuin-session";
